54a02964aa636b239ba3816b134a9228bfeed7cd
ahmadsarabi <ahmadsarabi26@gmail.com>
2023-12-01 12:55:19 -0800

    new changes2

--- index.js
+++ index.js
@@ -1,6 +1,1 @@
-   let name='ahmad'
-   console.log('this is ahmad')
-    console.log('this is 1')
-    console.log('this is 2')
-    console.log(name)
-    console.log(2+3)
+document.getElementById("root").innerHTML="adadad"
